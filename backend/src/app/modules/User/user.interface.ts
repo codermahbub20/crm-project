6e3cff5db12d542b58715d9dfc39dec502a5b49f
codermahbub20 <mkrefat5@gmail.com>
2025-04-24 21:21:45 +0600

    User interface and user constant file created

---
+++ backend/src/app/modules/User/user.interface.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+import { Model } from 'mongoose';
+import { USER_ROLE } from './user.constant';
+
+export interface TUser {
+  name: string;
+  email: string;
+  password: string;
+  role: 'admin' | 'user';
+  isBlocked: boolean;
+}
+
+export interface UserModel extends Model<TUser> {
+  isPasswordMatched(
+    plainTextPassword: string,
+    hashedPassword: string,
+  ): Promise<boolean>;
+  isUserBlocked(userEmail: string): Promise<TUser | null>;
+  isUserExistByEmail(email: string): Promise<TUser | null>;
+}
+
+export type TUserRole = keyof typeof USER_ROLE;
