25b0e942c64539238f9243fcedab3a10a3f79cb0
codermahbub20 <mkrefat5@gmail.com>
2025-04-25 02:18:39 +0600

    Client model created

--- backend/src/app/modules/Client/client.interface.ts
+++ backend/src/app/modules/Client/client.interface.ts
@@ -1,7 +1,15 @@
-export interface TUser {
+export interface TProject {
+  title: string;
+  budget: number;
+  deadline: Date;
+  status: 'Ongoing' | 'Completed' | 'Pending';
+}
+
+export interface TClient {
   name: string;
   email: string;
   phone: string;
   company?: string;
   notes?: string;
+  projects?: TProject[];
 }
---
+++ backend/src/app/modules/Client/client.model.ts
@@ -0,0 +1,24 @@
+import { Document, model, Schema } from 'mongoose';
+import { TClient } from './client.interface';
+
+const ProjectSchema = new Schema({
+  title: { type: String, required: true },
+  budget: { type: Number, required: true },
+  deadline: { type: Date, required: true },
+  status: {
+    type: String,
+    enum: ['Ongoing', 'Completed', 'Pending'],
+    required: true,
+  },
+});
+
+const ClientSchema = new Schema<TClient & Document>({
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  company: { type: String },
+  notes: { type: String },
+  projects: [ProjectSchema],
+});
+
+export const User = model<TClient & Document>('Client', ClientSchema);
