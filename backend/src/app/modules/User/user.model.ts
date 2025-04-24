8dcc7a592dcfdf1f1bc94288c2760992548ce8ca
codermahbub20 <mkrefat5@gmail.com>
2025-04-24 21:27:51 +0600

    User model created

---
+++ backend/src/app/modules/User/user.model.ts
@@ -0,0 +1,71 @@
+import { model, Schema } from 'mongoose';
+import { TUser, UserModel } from './user.interface';
+import config from '../../config';
+import bcrypt from 'bcrypt';
+
+const userSchema = new Schema<TUser, UserModel>(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ['admin', 'user'],
+      default: 'user',
+    },
+    isBlocked: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+userSchema.pre('save', async function (next) {
+  const user = this; // doc
+  // hashing password and save into DB
+  user.password = await bcrypt.hash(
+    user.password,
+    Number(config.bcrypt_salt_rounds),
+  );
+  next();
+});
+
+// set '' after saving password
+userSchema.post('save', function (doc, next) {
+  doc.password = '';
+  next();
+});
+
+userSchema.statics.isPasswordMatched = async function (
+  plainTextPassword,
+  hashedPassword,
+) {
+  return await bcrypt.compare(plainTextPassword, hashedPassword);
+};
+
+userSchema.statics.isUserBlocked = async function (userEmail: string) {
+  const user = await this.findOne({ email: userEmail, isBlocked: true });
+  return user;
+};
+
+userSchema.statics.isUserExistByEmail = async function (
+  email: string,
+): Promise<TUser | null> {
+  const user = await this.findOne({ email }).select('+password');
+  return user;
+};
+
+export const User = model<TUser, UserModel>('User', userSchema);
