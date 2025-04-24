6b486184364ff69c1687809efeb7775f1e6634c9
codermahbub20 <mkrefat5@gmail.com>
2025-04-25 00:27:11 +0600

    Auth Interface created

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,11 +1,16 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import router from './app/routes';
 
 const app: Application = express();
 
 // parsers use
 app.use(express.json());
 app.use(cors());
+
+// routes use
+// application routes
+app.use('/api', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello CRM World!');
