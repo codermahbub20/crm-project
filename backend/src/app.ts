3493aa5ddeed27fc2240caef1b7f7fb08166b20b
codermahbub20 <mkrefat5@gmail.com>
2025-04-24 20:04:59 +0600

    App.ts file created

---
+++ backend/src/app.ts
@@ -0,0 +1,14 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+const app: Application = express();
+
+// parsers use
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello CRM World!');
+});
+
+export default app;
